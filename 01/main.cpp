934eb76999ae2c16b319b7a948224a7bcd29f2d6
Jan Tacakiewicz <jan.tacakiewicz@protonmail.com>
2026-03-15 21:11:17 +0100

    01: parametrized

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,4 +1,5 @@
-// g++ main.cpp -o run && ./run
+// g++ main.cpp -o run && ./run N M
+#include <cassert>
 #include <functional>
 #include <iostream>
 #include <memory>
@@ -7,11 +8,9 @@
 #include <thread>
 #include <vector>
 
-#define MAX_N 10
-#define MAX_M 10
 std::mutex L;
-std::vector<int> acct = std::vector<int>(MAX_M, 0);
-std::vector<std::unique_ptr<std::mutex>> Lfine = std::vector<std::unique_ptr<std::mutex>>(MAX_M);
+std::vector<int> acct;
+std::vector<std::unique_ptr<std::mutex>> Lfine;
 
 void move_broad(int n, int i, int j) {
     L.lock();
@@ -42,10 +41,10 @@ void move_finegrained_safe(int n, int i, int j) {
     Lfine[mi]->unlock();
 }
 
-void move_couple(int id, int transfers_count, std::function<void(int, int, int)> move_func) {
+void move_couple(int id, int transfers_count, int account_range, std::function<void(int, int, int)> move_func) {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dist(0, MAX_M-1);
+    std::uniform_int_distribution<> dist(0, account_range-1);
     std::uniform_int_distribution<> transferred(1, 100);
 
     for (int i = 0; i < transfers_count; i++) {
@@ -59,16 +58,27 @@ void move_couple(int id, int transfers_count, std::function<void(int, int, int)>
     }
 }
 
-int main() {
+int main(int argc, char** argv) {
+    int n = std::stoi(argv[1]);
+    int m = std::stoi(argv[2]);
+    if (argc < 3 ) {
+        std::cout << "Usage: program [N threads] [M accounts]\n";
+        return 0;
+    }
+
+    acct = std::vector<int>(m, 0);
+    Lfine = std::vector<std::unique_ptr<std::mutex>>(m);
+
     for (auto& l : Lfine) {
         l = std::make_unique<std::mutex>();
     }
     std::vector<std::unique_ptr<std::thread> > threads;
-    for (int i = 0; i < MAX_N; i++) {
+    for (int i = 0; i < n; i++) {
         threads.emplace_back(std::make_unique<std::thread>(
             move_couple,
             i,
             100,
+            m,
             move_finegrained_safe
         ));
     }
@@ -76,4 +86,6 @@ int main() {
         t->join();
     }
     int sum = std::reduce(std::begin(acct), std::end(acct));
+    assert(sum == 0);
+    return 0;
 }
